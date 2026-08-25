7e197b59e3483db36dac9d879d1812595ff94485
Fabio Castro Morffi <fabiocastromorffi@gmail.com>
2026-08-25 14:46:57 -0400

    Initial commit

---
+++ opts.nix
@@ -0,0 +1,14 @@
+{
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+    have_nerd_font = true;
+  };
+
+  opts = {
+    number = true;
+    sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"; #what gets remembered when clossing session
+    mouse = "a"; #in what mode responds to mouse input
+    showmode = false; #shows mode in command line (already shown by atc line)
+  };
+}
